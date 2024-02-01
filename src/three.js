3005cf59c79cf566fe8c764a11eff46ec94a77a5
Lenni <guentherhanslphdyt@gmail.com>
2024-02-01 09:51:17 +0100

    test import

--- src/three.js
+++ src/three.js
@@ -1,6 +1,6 @@
 import * as THREE from "three";
 window.THREE = THREE;
-require("three/examples/js/controls/OrbitControls.js");
+import "three/examples/js/controls/OrbitControls.js";
 
 let OrbitControls = THREE.OrbitControls;
 export default THREE;
